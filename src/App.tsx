714f905a1435c9a96f67717c9dcc01a833101531
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2026-01-31 18:34:33 +0530

    xyflow setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
 import './App.css'
+import { useState, useCallback } from 'react';
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, type NodeChange, type Node, type Edge, type EdgeChange, type Connection } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
 
-function App() {
-	return <div>Hello</div>
+const initialNodes = [
+	{ id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+	{ id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
+interface CustomNode extends Node {
+	id: string;
+	position: { x: number, y: number };
+	data: { label: string; }
+};
+
+
+interface CustomEdge extends Edge {
+
+};
+
+
+export default function App() {
+	const [nodes, setNodes] = useState(initialNodes);
+	const [edges, setEdges] = useState(initialEdges);
+
+	const onNodesChange = useCallback(
+		(changes: NodeChange<CustomNode>[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+		[],
+	);
+
+	const onEdgesChange = useCallback(
+		(changes: EdgeChange<CustomEdge>[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+		[],
+	);
+	const onConnect = useCallback(
+		(params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+		[],
+	);
+
+	return (
+		<div style={{ width: '100vw', height: '100vh' }}>
+			<ReactFlow
+				nodes={nodes}
+				edges={edges}
+				onNodesChange={onNodesChange}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
+				fitView
+			/>
+		</div>
+	);
 }
-
-export default App
